3192a11bff18d0fe49e89af39d8812b89255439d
Samuel Haluza <samuel.haluza2@gmail.com>
2026-04-30 17:23:33 +0200

    Exporty: Integrácia CSV a XLSX

---
+++ Modules/IdentityAccess/app/Exports/UserExport.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Modules\IdentityAccess\Exports;
+
+use Modules\IdentityAccess\Models\User;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+
+class UserExport implements FromQuery, WithHeadings, WithMapping, ShouldAutoSize, WithEvents
+{
+    protected $query;
+
+    public function __construct($query = null)
+    {
+        $this->query = $query;
+    }
+
+    public function query()
+    {
+        return $this->query ?? User::query()->select(['id', 'name', 'surname', 'email', 'status_id', 'created_at']);
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Meno',
+            'Priezvisko',
+            'Email',
+            'Status',
+            'Vytvorené'
+        ];
+    }
+
+    public function map($user): array
+    {
+        return [
+            $user->id,
+            $user->name,
+            $user->surname,
+            $user->email,
+            optional($user->status)->name,
+            $user->created_at?->toDateTimeString(),
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $event->sheet->getStyle('A1:F1')->getFont()->setBold(true);
+            },
+        ];
+    }
+}
